5ec5b78df80fdfd4c805997b180271dfdeaa5c24
DevBoiAgru <79085233+DevBoiAgru@users.noreply.github.com>
2026-02-07 21:32:37 +0530

    Fix

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, questions, users
